498736b91a0434541a9b7b16040009836b2be16f
Alon Stern <Alon.Stern@gaea6.ncrc.gov>
2015-07-22 17:55:00 -0400

    adding print statements:

--- icebergs.F90
+++ icebergs.F90
@@ -1601,8 +1601,8 @@ integer :: stderrunit
   u1=uvel1*dxdl1; v1=vvel1*dydl
   !call accel(bergs, berg, i, j, xi, yj, lat1, uvel1, vvel1, uvel1, vvel1, dt_2, ax1, ay1, axn1, ayn1, bxn, byn, Runge_not_Verlet) !axn,ayn, bxn, byn ,Runge_not_Verlet  - Added by Alon
 
-print *,  i, j, xi, yj, lat1, uvel1, vvel1, uvel1, vvel1, dt, ax1, ay1, axn1, ayn1, bxn, byn, Runge_not_Verlet
-  call accel(bergs, berg, i, j, xi, yj, lat1, uvel1, vvel1, uvel1, vvel1, dt, ax1, ay1, axn1, ayn1, bxn, byn, Runge_not_Verlet) !Note change to dt. Markpoint_1
+print *,  i, j, xi, yj, lat1, uvel1, vvel1, uvel1, vvel1, dt_2, axn1, ayn1, Runge_not_Verlet
+  call accel(bergs, berg, i, j, xi, yj, lat1, uvel1, vvel1, uvel1, vvel1, dt_2, ax1, ay1, axn1, ayn1, bxn, byn, Runge_not_Verlet) !Note change to dt. Markpoint_1
 print *, ax1, ay1, axn, ayn, bxn, byn
 stop
   if (on_tangential_plane) call rotvec_to_tang(lon1,ax1,ay1,xddot1,yddot1)
