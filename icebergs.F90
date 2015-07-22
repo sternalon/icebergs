3ccdd8aa5c6297fec75225cfaf22dc8093f51767
Alon Stern <Alon.Stern@gaea6.ncrc.gov>
2015-07-22 17:09:48 -0400

    almost nothing edit

--- icebergs.F90
+++ icebergs.F90
@@ -1604,7 +1604,7 @@ integer :: stderrunit
   if (on_tangential_plane) call rotvec_to_tang(lon1,ax1,ay1,xddot1,yddot1)
   if (on_tangential_plane) call rotvec_to_tang(lon1,axn1,ayn1,xddot1n,yddot1n) !Alon
  
-print *, ax, ay
+print *, ax1, ay1
 stop
  
   !  X2 = X1+dt/2*V1 ; V2 = V1+dt/2*A1; A2=A(X2)
